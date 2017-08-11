bd0bce4c25c1a91dc7565012c4ec8a8f47190dd7
Jane <jane@Jane-MacBook-Pro.local>
2017-08-11 23:14:46 +0800

    init

---
+++ src/app.js
@@ -0,0 +1,18 @@
+// var header = require('./component/header');
+
+import _ from 'lodash';
+// import './css/app.css';
+
+'use strict';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+export default class H1 extends React.Component {
+  render() {
+    return (
+      <h1>ggagagaga</h1>
+    )
+  }
+}
+
+ReactDOM.render(<H1/>, document.getElementById('root'));
---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+var debug = process.env.NODE_ENV !== "production";
+var webpack = require('webpack');
+var path = require('path');
+
+module.exports = {
+  context: path.join(__dirname),
+  devtool: "inline-sourcemap",
+  entry: {
+    app: ["./src/app.js"]
+  },
+  module: {
+    loaders: [{
+      test: /\.js?$/,
+      exclude: /(node_modules)/,
+      loader: 'babel-loader',
+      query: {
+        presets: ['react', 'es2015'],
+        plugins: ['react-html-attrs'] //添加组件的插件配置
+      }
+    }, {
+      //下面是使用ant-design的配置文件
+      test: /\.css$/,
+      loader: 'style-loader!css-loader'
+    }]
+  },
+  output: {
+    path: __dirname,
+    filename: "./dist/bundleApp.js"
+  },
+  devServer: {
+    // contentBase: './dist',
+    // hot: true
+  },
+  plugins: [
+    // new webpack.optimize.DedupePlugin(),
+    // new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.optimize.UglifyJsPlugin({
+      beautify: false,
+      mangle: {
+        screw_ie8: true,
+        keep_fnames: true
+      },
+      compress: {
+        screw_ie8: true
+      },
+      comments: false
+    }),
+    new webpack.LoaderOptionsPlugin({
+      minimize: true,
+      debug: false
+    }),
+  ],
+};
